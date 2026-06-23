836bc75a10ff0dbe9df969af3e402e2591e8912a
surya <suryaprakashn45831@gmail.com>
2026-06-23 20:41:57 +0530

    Added Move Zeroes using Two Pointer approach

---
+++ Two-Pointer/MoveZeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int l=0;
+        for(int r=0;r<n;r++)
+        {
+            if(nums[r]!=0)
+            {
+                int t=nums[l];
+                nums[l]=nums[r];
+                nums[r]=t;
+                l++;
+            }
+        }
+    }
+}
