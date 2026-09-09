a9904ab758181971d1c89c637b386193e9703d7f
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:11:36 +0530

    Solved LeetCode #132: Palindrome Partitioning II [C++]

---
+++ String/132-Palindrome-Partitioning-Ii.cpp
@@ -0,0 +1,31 @@
+// Problem Number: 132
+// Problem Name: Palindrome Partitioning II
+// Difficulty: Hard
+// Topic: String, Dynamic Programming
+
+class Solution {
+public:
+    int minCut(string s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int len = s.size();  
+3:            int D[len+1];  
+4:            bool P[len][len];  
+5:            //the worst case is cutting by each char  
+6:            for(int i = 0; i <= len; i++)   
+7:            D[i] = len-i;  
+8:            for(int i = 0; i < len; i++)  
+9:            for(int j = 0; j < len; j++)  
+10:            P[i][j] = false;  
+11:            for(int i = len-1; i >= 0; i--){  
+12:                 for(int j = i; j < len; j++){  
+13:                      if(s[i] == s[j] && (j-i<2 || P[i+1][j-1])){  
+14:                           P[i][j] = true;  
+15:                           D[i] = min(D[i],D[j+1]+1);  
+16:                      }  
+17:                 }  
+18:            }  
+19:            return D[0]-1;  
+        
+    }
+};
