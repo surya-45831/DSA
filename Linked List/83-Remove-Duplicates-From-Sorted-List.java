3109c9cdcd7305085c6df220af7cbc8473e09c04
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:14:31 +0530

    Solved LeetCode #83: Remove Duplicates from Sorted List [Java]

---
+++ Linked List/83-Remove-Duplicates-From-Sorted-List.java
@@ -0,0 +1,40 @@
+// Problem Number: 83
+// Problem Name: Remove Duplicates from Sorted List
+// Difficulty: Easy
+// Topic: Linked List
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode c = head;
+        ListNode n = head.next;
+        while(n!=null) {
+            if(c.val == n.val) {
+                n = n.next;
+            }else {
+                c.next = n;
+                c = n;
+                n = n.next;
+            }
+        }
+        if(c == head) {
+            c.next = n;
+        }
+        return head;
+    }
+}
