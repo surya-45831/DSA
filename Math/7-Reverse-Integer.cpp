28d3586fd9d4a1ecbf3582be1f786a24f382ea08
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:11:50 +0530

    Solved LeetCode #7: Reverse Integer [C++]

---
+++ Math/7-Reverse-Integer.cpp
@@ -0,0 +1,29 @@
+// Problem Number: 7
+// Problem Name: Reverse Integer
+// Difficulty: Medium
+// Topic: Math
+
+class Solution {
+public:
+    int reverse(int x) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(x/10==0)return x;
+        bool flag=x>0;
+        if(!flag)x=-x;
+        vector<int> digits;
+        while(x!=0){
+            digits.push_back(x%10);
+            x/=10;
+        }
+        int ret=0;
+         
+        for(int i=0;i<digits.size();i++){
+           ret*=10;
+           ret+=digits[i];
+            
+        }
+        if(!flag)ret=-ret;
+        return ret;
+    }
+};
