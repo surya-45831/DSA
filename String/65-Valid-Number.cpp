49a1fca49582375f8cfad15dcc0afd664dd29b11
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:15:12 +0530

    Solved LeetCode #65: Valid Number [C++]

---
+++ String/65-Valid-Number.cpp
@@ -0,0 +1,17 @@
+// Problem Number: 65
+// Problem Name: Valid Number
+// Difficulty: Hard
+// Topic: String
+
+class Solution {
+public:
+    bool isNumber(const char *s) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        for (char *p = s; *p != 0; ++p){
+            if ((*p < 48 || *p > 57) && *p != 46 && *p != 32 && *p != 101) return false;
+            else if ((p == s || *(p + 1) == 0) && (*p = 101 || *p == 46)) return false;
+        }
+    return true;
+    }
+};
