0c8093a61bef11ec7d786daed932ede65de5bc3c
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:11:33 +0530

    Solved LeetCode #129: Sum Root to Leaf Numbers [C++]

---
+++ Tree/129-Sum-Root-To-Leaf-Numbers.cpp
@@ -0,0 +1,36 @@
+// Problem Number: 129
+// Problem Name: Sum Root to Leaf Numbers
+// Difficulty: Medium
+// Topic: Tree, Depth-First Search, Binary Tree
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void sumFunction(TreeNode *root, int *sum,int s){
+        if(root==NULL)
+            return;
+        else if (root->left==NULL && root->right==NULL)
+            *sum=(*sum)+s*10+root->val;
+        else{
+            sumFunction(root->left,sum,s*10+root->val);
+            sumFunction(root->right,sum,s*10+root->val);
+        }
+    } 
+    int sumNumbers(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int sum=0;
+        int s=0;
+        sumFunction(root,&sum,s);
+        return sum;
+        
+    }
+};
