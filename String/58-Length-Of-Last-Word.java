07659d069f89b3b0428fc39ce8dbe6964c93ecc7
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:11:44 +0530

    Solved LeetCode #58: Length of Last Word [Java]

---
+++ String/58-Length-Of-Last-Word.java
@@ -0,0 +1,13 @@
+// Problem Number: 58
+// Problem Name: Length of Last Word
+// Difficulty: Easy
+// Topic: String
+
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        String[] str = s.split(" +");
+        return str[str.length-1].length();
+    }
+}
