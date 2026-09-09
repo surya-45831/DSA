984154b4188e80f6533c87453f5c417fd7d388bd
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:14:19 +0530

    Solved LeetCode #124: Binary Tree Maximum Path Sum [C++]

---
+++ Dynamic Programming/124-Binary-Tree-Maximum-Path-Sum.cpp
@@ -0,0 +1,35 @@
+// Problem Number: 124
+// Problem Name: Binary Tree Maximum Path Sum
+// Difficulty: Hard
+// Topic: Dynamic Programming, Tree, Depth-First Search, Binary Tree, DP on Trees
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int cmax = 0;
+        int maxValue = INT_MIN;
+        maxHelper(root, cmax, maxValue);
+        return maxValue;
+    }
+    
+    void maxHelper(TreeNode *root, int &cmax, int &maxValue){
+        if(root){
+            int left, right;
+            maxHelper(root->left, left, maxValue);
+            maxHelper(root->right, right, maxValue);
+            cmax = max(cmax, max(left, right) + root->val);
+            maxValue = max(left + root->val + right, max(maxValue, cmax));
+        }
+    }
+};
