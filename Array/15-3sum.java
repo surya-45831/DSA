c1e054e0a4188c116a6595b544e7e1eca57fe7c4
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:13:56 +0530

    Solved LeetCode #15: 3Sum [Java]

---
+++ Array/15-3sum.java
@@ -0,0 +1,35 @@
+// Problem Number: 15
+// Problem Name: 3Sum
+// Difficulty: Medium
+// Topic: Array, Two Pointers, Sorting
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        Arrays.sort(num);
+        for(int i = 0; i<num.length; i++) {
+            int j = i+1;
+            int k = num.length-1;
+            while(j<k&&j<num.length) {
+                    int sum = num[i] + num[j] + num[k];
+                    if(sum==0) {
+                        ArrayList<Integer> tmp = new ArrayList<Integer>();
+                        tmp.add(num[i]);
+                        tmp.add(num[j]);
+                        tmp.add(num[k]);
+                        res.add(tmp);
+                        j++;
+                        k--;
+                    } else if(sum>0) {
+                        k--;
+                    } else {
+                        j++;
+                    }
+                }
+            }
+        return res;
+        }
+    }
+}
