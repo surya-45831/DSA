07e58fd5006ab5e4ae86b2dc16d42c2cf7549e19
surya-45831 <suryaprakashn45831@gmail.com>
2026-09-09 10:14:12 +0530

    Solved LeetCode #55: Jump Game [Java]

---
+++ Array/55-Jump-Game.java
@@ -0,0 +1,26 @@
+// Problem Number: 55
+// Problem Name: Jump Game
+// Difficulty: Medium
+// Topic: Array, Dynamic Programming, Greedy
+
+public class Solution {
+    public boolean canJump(int[] A) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if (A.length == 0) return false;
+        boolean[] array = new boolean[A.length];
+        array[0] = true;
+        
+        for (int i=0; i<A.length; i++) {
+            if (array[i] == false) continue;
+            else {
+                int jum = A[i];
+                for (int j = 1; j<=jum; j++) {
+                    if (i+j < A.length) array[i+j] = true;
+                    else break;
+                }
+            }
+        }
+        return array[A.length-1];
+    }
+}
